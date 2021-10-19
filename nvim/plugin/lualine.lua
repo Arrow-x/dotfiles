b1d6a14bea99d7b35f8bb2b86db1c9d020f4e9bf
Arrow-x <Houssem.kun26@gmail.com>
2021-10-19 19:06:49 +0100

    add lsp status to lualine

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -1,4 +1,17 @@
 require('lualine').setup {
     options = {theme = 'gruvbox_material'};
     extensions = {'nvim-tree','fugitive','fzf'};
+    sections = {
+        lualine_b = {{'branch', icon = ''}, 'diff'},
+        lualine_c = {'filename', {"diagnostics", sources = {"nvim_lsp"}}},
+
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+        }
+    }
 }
