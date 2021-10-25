2fb60c175ff866ee5979ac5e05fbf2ba0025c004
Arrow-x <Houssem.kun26@gmail.com>
2021-10-25 23:54:34 +0100

    added an install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+mkdir -p $HOME/.local/share/nvim/plugged
+mkdir -p $HOME/.cache/nvim/undodir
+ln -s $PWD/nvim/ $HOME/.config
+ln -s $PWD/zsh/.config/zsh/ $HOME/.config
+ln -s $PWD/kitty/ $HOME/.config
