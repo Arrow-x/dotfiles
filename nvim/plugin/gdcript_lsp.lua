7f462c9944627c3d2fafa7bdaba8f7db7d517e28
Arrow-x <Houssem.kun26@gmail.com>
2021-10-28 18:53:00 +0100

    cmp for gdscript

--- nvim/plugin/gdcript_lsp.lua
+++ nvim/plugin/gdcript_lsp.lua
@@ -1,1 +1,4 @@
-require'lspconfig'.gdscript.setup{}
+require'lspconfig'.gdscript.setup{
+	capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+}
+
