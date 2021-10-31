e1e8e78de30b72a30f3eeb86e0549f02118f5971
Arrow-x <Houssem.kun26@gmail.com>
2021-10-31 15:45:35 +0100

    fixed the zsh path

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mkdir -p $HOME/.local/share/nvim/plugged
 mkdir -p $HOME/.cache/nvim/undodir
 ln -s $PWD/nvim/ $HOME/.config
-ln -s $PWD/zsh/.config/zsh/ $HOME/.config
+ln -s $PWD/zsh/ $HOME/.config
 ln -s $PWD/kitty/ $HOME/.config
 ln -s $PWD/ranger/ $HOME/.config
