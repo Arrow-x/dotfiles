d63e22778487b8705619754bf8f888b83a037930
Arrow-x <Houssem.kun26@gmail.com>
2021-11-06 00:31:12 +0100

    add cmp to vim command line

--- nvim/plugin/nvim-cmp.lua
+++ nvim/plugin/nvim-cmp.lua
@@ -40,5 +40,18 @@ cmp.setup{
 
     experimental = {
         native_menu = false,
-    }
+    },
+
+	-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+	cmp.setup.cmdline('/', { sources =  { name = 'buffer' } }),
+
+	  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+	cmp.setup.cmdline(':', {
+		sources = cmp.config.sources({
+			{ name = 'path' }
+		}, {
+				{ name = 'cmdline' }
+			})
+	})
+
 }
