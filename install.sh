89e0c4c7eefd363dc7440bb214442c7b0c36c649
Arrow-x <Houssem.kun26@gmail.com>
2021-11-08 02:27:51 +0100

    install i3 configs

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 mkdir -p $HOME/.local/share/nvim/plugged
 mkdir -p $HOME/.cache/nvim/undodir
+
 ln -s $PWD/nvim/ $HOME/.config
 ln -s $PWD/zsh/ $HOME/.config
 ln -s $PWD/kitty/ $HOME/.config
 ln -s $PWD/ranger/ $HOME/.config
 ln -s $PWD/.xprofile $HOME/
+ln -s $PWD/i3 $HOME/.config
+
+./$HOME/.xprofile
