658708816ec26ef098763245c234e3f7b3f9f60d
Arrow-x <Houssem.kun26@gmail.com>
2022-03-31 17:40:51 +0100

    permenent Wacom settings

---
+++ Wacom/wacom-config.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+for i in $(seq 10); do
+    if xsetwacom list devices | grep -q Wacom; then
+        break
+		echo "Break"
+    fi
+    sleep 1
+	echo "Sleep 1"
+done
+
+list=$(xsetwacom list devices)
+pad=$(echo "${list}" | awk '/pad/{print $9}')
+stylus=$(echo "${list}" | xsetwacom list devices | awk '/stylus/{print $9}')
+
+# if [ -z "${pad}" ]; then
+# 	echo "exiting"
+#     exit 0
+# fi
+
+# configure the buttons on ${stylus} with your xsetwacom commands...
+#xsetwacom set "${stylus}" Button 2 11
+#...
+echo "about to set"
+xsetwacom set "${stylus}" area 0 0 21600 12150
