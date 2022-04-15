d91f7a42eaf96f946995df960fbe8bf28268f5a9
Arrow-x <Houssem.kun26@gmail.com>
2022-04-15 18:30:40 +0100

    nvim-cmp new settings

--- nvim/plugin/nvim-cmp.lua
+++ nvim/plugin/nvim-cmp.lua
@@ -15,15 +15,15 @@ cmp.setup{
         end,
     },
 
-    mapping = {
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    },
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
 
-    sources = {
+    sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'nvim_lua' },
         { name = 'luasnip' },
@@ -32,21 +32,28 @@ cmp.setup{
         { name = 'calc' },
         { name = 'spell' },
         { name = 'look'},
-    },
+    }),
 
     formatting= {
         format = lspkind.cmp_format({with_text = true,})
     },
 
-    experimental = {
-        native_menu = false,
-    },
+	cmp.setup.filetype('gitcommit', {
+		sources = cmp.config.sources({
+			{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+			}, {
+				{ name = 'buffer' },
+		})
+	}),
 
 	-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-	cmp.setup.cmdline('/', { sources =  { name = 'buffer' } }),
+	cmp.setup.cmdline('/', {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources =  { name = 'buffer' } }),
 
 	  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 	cmp.setup.cmdline(':', {
+		mapping = cmp.mapping.preset.cmdline(),
 		sources = cmp.config.sources({
 			{ name = 'path' }
 		}, {
