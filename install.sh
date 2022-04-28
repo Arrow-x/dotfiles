94e6f634cd7cf1c98c4d5e41b9cc1f0e997b8389
Arrow-x <Houssem.kun26@gmail.com>
2022-04-28 02:38:08 +0100

    removed ranger config installation

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ mkdir -p $HOME/.cache/nvim/undodir
 ln -s $PWD/nvim/ $HOME/.config
 ln -s $PWD/zsh/ $HOME/.config
 ln -s $PWD/kitty/ $HOME/.config
-ln -s $PWD/ranger/ $HOME/.config
 ln -s $PWD/.xprofile $HOME/
 ln -s $PWD/i3 $HOME/.config
 ln -s $PWD/rofi $HOME/.config
