f0c89eb7bca18354d1d9248aeb214963b57867b2
Arrow-x <Houssem.kun26@gmail.com>
2022-05-01 14:14:18 +0100

    Formatting nvim options

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,48 +1,48 @@
 -- :help options
-o = vim.opt
+O = vim.opt
 
-o.clipboard = ""
-o.cmdheight = 1 -- more space in the neovim command line for displaying messages
-o.completeopt = { "menuone", "noselect" } -- mostly just for cmp
-o.conceallevel = 0 -- so that `` is visible in markdown files
-o.fileencoding = "utf-8" -- the encoding written to a file
+O.clipboard = ""
+O.cmdheight = 1 -- more space in the neovim command line for displaying messages
+O.completeopt = { "menuone", "noselect" } -- mostly just for cmp
+O.conceallevel = 0 -- so that `` is visible in markdown files
+O.fileencoding = "utf-8" -- the encoding written to a file
 -- o.mouse = "a"                             -- allow the mouse to be used in neovim
-o.showmode = false
-o.pumheight = 10 -- pop up menu height
-o.showtabline = 2 -- always show tabs
-o.smartcase = true -- smart case
-o.splitbelow = true -- force all horizontal splits to go below current window
-o.splitright = true -- force all vertical splits to go to the right of current window
-o.hidden = true -- buffers are kept open in the background
-o.errorbells = false
-o.termguicolors = true -- set term gui colors (most terminals support this)
-o.timeoutlen = 1000 -- time to wait for a mapped sequence to complete (in milliseconds)
-o.undofile = true -- enable persistent undo
-o.backup = false -- creates a backup file
-o.swapfile = false -- creates a swapfile
-o.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-o.updatetime = 300 -- faster completion (4000ms default)
-o.shiftwidth = 4 -- the number of spaces inserted for each indentation
-o.cursorline = false -- highlight the current line
-o.number = true -- set numbered lines
-o.relativenumber = true -- set relative numbered lines
-o.numberwidth = 2 -- set number column width to 2 {default 4}
-o.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
-o.wrap = false -- display lines as one long line
-o.scrolloff = 8 -- is one of my fav
-o.sidescrolloff = 8
+O.showmode = false
+O.pumheight = 10 -- pop up menu height
+O.showtabline = 2 -- always show tabs
+O.smartcase = true -- smart case
+O.splitbelow = true -- force all horizontal splits to go below current window
+O.splitright = true -- force all vertical splits to go to the right of current window
+O.hidden = true -- buffers are kept open in the background
+O.errorbells = false
+O.termguicolors = true -- set term gui colors (most terminals support this)
+O.timeoutlen = 1000 -- time to wait for a mapped sequence to complete (in milliseconds)
+O.undofile = true -- enable persistent undo
+O.backup = false -- creates a backup file
+O.swapfile = false -- creates a swapfile
+O.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+O.updatetime = 300 -- faster completion (4000ms default)
+O.shiftwidth = 4 -- the number of spaces inserted for each indentation
+O.cursorline = false -- highlight the current line
+O.number = true -- set numbered lines
+O.relativenumber = true -- set relative numbered lines
+O.numberwidth = 2 -- set number column width to 2 {default 4}
+O.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
+O.wrap = false -- display lines as one long line
+O.scrolloff = 8 -- is one of my fav
+O.sidescrolloff = 8
 -- o.guifont = "monospace:h17"               -- the font used in graphical neovim applications
 
-o.hlsearch = true -- highlight all matches on previous search pattern
-o.incsearch = true
-o.ignorecase = false -- ignore case in search patterns
+O.hlsearch = true -- highlight all matches on previous search pattern
+O.incsearch = true
+O.ignorecase = false -- ignore case in search patterns
 vim.cmd("highlight Normal guibg=none")
 
-o.smartindent = true -- make indenting smarter again
-o.expandtab = false -- convert tabs to spaces
-o.tabstop = 4 -- insert 4 spaces for a tab
+O.smartindent = true -- make indenting smarter again
+O.expandtab = false -- convert tabs to spaces
+O.tabstop = 4 -- insert 4 spaces for a tab
 
-o.shortmess:append("c")
+O.shortmess:append("c")
 
 vim.cmd("set whichwrap+=<,>,[,],h,l")
 vim.cmd([[set iskeyword+=-]])
