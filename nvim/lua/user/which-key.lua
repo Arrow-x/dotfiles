bf46a4b9310e4483a06b985ede232a0f96d7fdcc
Arrow-x <Houssem.kun26@gmail.com>
2022-05-01 14:10:48 +0100

    Use Horizontal toggleterm terminal

--- nvim/lua/user/which-key.lua
+++ nvim/lua/user/which-key.lua
@@ -161,13 +161,14 @@ local mappings = {
 
 	t = {
 		name = "Terminal",
-		n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
-		u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
+		-- n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
+		-- u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
 		t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
 		p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
-		f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
+		g = {"<cmd>lua _LAZYGIT_TOGGLE()<cr>", "Lazygit"},
+		-- f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
 		h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
-		v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
+		-- v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
 	},
 }
 
