6e79504f108f3d3869ec752ef52ccad53668518d
Arrow-x <Houssem.kun26@gmail.com>
2022-05-19 02:34:40 +0100

    delete the nvim config

--- nvim/.config/nvim/lua/user/dap/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "dap")
-if not status_ok then
-	return
-end
-
-require("user.dap.dap-python")
-require("user.dap.dap-ui")
-require("user.dap.dap-ui-virtualtext")
--- nvim/.config/nvim/lua/user/lsp/settings/gdscript.lua
+++
@@ -1,5 +0,0 @@
-local opts = {
-	on_attach = require("user.lsp.handlers").on_attach,
-	capabilities = require("user.lsp.handlers").capabilities,
-}
-require'lspconfig'.gdscript.setup{opts}
