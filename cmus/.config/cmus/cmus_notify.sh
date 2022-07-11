b3b3bde96909e9150610943ff1608999e35ea449
Arrow-x <Houssem.kun26@gmail.com>
2022-07-11 22:48:02 +0100

    if there is no meta date on the audio file the notification will show the basename

--- cmus/.config/cmus/cmus_notify.sh
+++ cmus/.config/cmus/cmus_notify.sh
@@ -1,13 +1,28 @@
 #! /bin/sh
-
-# TODO check if any the fields are nill
+# TODO a default icon when no album art
 cmus_query="$(cmus-remote --query)"
 
 title="$(echo "$cmus_query" | grep "title" | sed "s/tag title //g")"
 artist="$(echo "$cmus_query" | grep "tag \bartist" | sed "s/tag artist //g")"
 album="$(echo "$cmus_query" | grep "album\b" | sed 's/tag album //g')"
 
-echo "$cmus_query" | grep "file" | sed "s/file //g" | sed 's/\"/\\"/g' | sed 's/$/\"/;s/^/"/g' | xargs -I {} ffmpegthumbnailer -i "{}" -o ~/.cache/cmus_notification_cover.jpg 
+echo "$cmus_query" | grep "file" | sed "s/file //g" | sed 's/"/\\"/g' | sed 's/$/"/;s/^/"/g' | xargs -I {} ffmpegthumbnailer -i "{}" -o ~/.cache/cmus_notification_cover.jpg 
+
+if [ -z "$artist" ];
+then
+	artist="Unknown"
+fi
+
+if [ -z "$album" ];
+then
+	album="Unknown"
+fi
+
+if [ -z "$title" ];
+then
+	title="$(echo "$cmus_query" | grep "file" | sed "s/file //g" | sed 's/"/\\"/g' | sed 's/$/"/;s/^/"/g' | xargs basename)"
+fi
+
 notify-send -u low --icon=~/.cache/cmus_notification_cover.jpg "Now Playing:" "<b>$title</b>\nby: <b>$artist</b> from: <b>$album</b>"
 
 rm ~/.cache/cmus_notification_cover.jpg
