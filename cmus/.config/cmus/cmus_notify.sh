7ae40210373f542568b97e363fae240826b414d2
Arrow-x <Houssem.kun26@gmail.com>
2022-07-11 12:10:40 +0100

    cleans up after it self

--- cmus/.config/cmus/cmus_notify.sh
+++ cmus/.config/cmus/cmus_notify.sh
@@ -8,7 +8,7 @@ title="$(echo "$cmus_query" | grep "title" | sed "s/tag title //g")"
 artist="$(echo "$cmus_query" | grep "tag \bartist" | sed "s/tag artist //g")"
 album="$(echo "$cmus_query" | grep "album\b" | sed 's/tag album //g')"
 
-rm ~/.cache/cover.jpg
+echo "$cmus_query" | grep "file" | sed "s/file //g" | sed 's/\"/\\"/g' | sed 's/$/\"/;s/^/"/g' | xargs -I {} ffmpegthumbnailer -i "{}" -o ~/.cache/cmus_notification_cover.jpg 
+notify-send -u low --icon=~/.cache/cmus_notification_cover.jpg "$title" "by: $artist from: $album"
 
-echo "$cmus_query" | grep "file" | sed "s/file //g" | sed 's/\"/\\"/g' | sed 's/$/\"/;s/^/"/g' | xargs -I {} ffmpegthumbnailer -i "{}" -o ~/.cache/cover.jpg 
-notify-send -u low --icon=~/.cache/cover.jpg "$title" "by: $artist from: $album"
+rm ~/.cache/cmus_notification_cover.jpg
