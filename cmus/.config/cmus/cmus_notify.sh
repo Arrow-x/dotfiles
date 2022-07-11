f975b97c7c0e09a4ff04d231e53ed50459390247
Arrow-x <Houssem.kun26@gmail.com>
2022-07-11 12:06:14 +0100

    cashe the resault of one cmus-remote call and use that

--- cmus/.config/cmus/cmus_notify.sh
+++ cmus/.config/cmus/cmus_notify.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 
-# TODO cash one call to cmus-remote and use that
 # TODO figure out how to do line breaks (and some color) in the notification message
 # TODO check if any the fields are nill
+cmus_query="$(cmus-remote --query)"
 
-title="$(cmus-remote --query | grep "title" | sed "s/tag title //g")"
-artist="$(cmus-remote --query | grep "tag \bartist" | sed "s/tag artist //g")"
-album="$(cmus-remote --query | grep "album\b" | sed 's/tag album //g')"
+title="$(echo "$cmus_query" | grep "title" | sed "s/tag title //g")"
+artist="$(echo "$cmus_query" | grep "tag \bartist" | sed "s/tag artist //g")"
+album="$(echo "$cmus_query" | grep "album\b" | sed 's/tag album //g')"
 
 rm ~/.cache/cover.jpg
 
-cmus-remote --query | grep "file" | sed "s/file //g" | sed 's/\"/\\"/g' | sed 's/$/\"/;s/^/"/g' | xargs -I {} ffmpegthumbnailer -i "{}" -o ~/.cache/cover.jpg 
+echo "$cmus_query" | grep "file" | sed "s/file //g" | sed 's/\"/\\"/g' | sed 's/$/\"/;s/^/"/g' | xargs -I {} ffmpegthumbnailer -i "{}" -o ~/.cache/cover.jpg 
 notify-send -u low --icon=~/.cache/cover.jpg "$title" "by: $artist from: $album"
