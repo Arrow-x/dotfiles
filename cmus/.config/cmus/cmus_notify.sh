7b4301d48947c9beadceeb079c1245b8d230248e
Arrow-x <Houssem.kun26@gmail.com>
2022-07-11 15:51:27 +0100

    html formating in cmus notification

--- cmus/.config/cmus/cmus_notify.sh
+++ cmus/.config/cmus/cmus_notify.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 
-# TODO figure out how to do line breaks (and some color) in the notification message
 # TODO check if any the fields are nill
 cmus_query="$(cmus-remote --query)"
 
@@ -9,6 +8,6 @@ artist="$(echo "$cmus_query" | grep "tag \bartist" | sed "s/tag artist //g")"
 album="$(echo "$cmus_query" | grep "album\b" | sed 's/tag album //g')"
 
 echo "$cmus_query" | grep "file" | sed "s/file //g" | sed 's/\"/\\"/g' | sed 's/$/\"/;s/^/"/g' | xargs -I {} ffmpegthumbnailer -i "{}" -o ~/.cache/cmus_notification_cover.jpg 
-notify-send -u low --icon=~/.cache/cmus_notification_cover.jpg "$title" "by: $artist from: $album"
+notify-send -u low --icon=~/.cache/cmus_notification_cover.jpg "Now Playing:" "<b>$title</b>\nby: <b>$artist</b> from: <b>$album</b>"
 
 rm ~/.cache/cmus_notification_cover.jpg
