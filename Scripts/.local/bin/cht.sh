08541ff7138356929ddc2cd8502dcd841a2f8bfb
Arrow-x <Houssem.kun26@gmail.com>
2022-07-23 15:15:11 +0100

    cht.sh and fzf script

---
+++ Scripts/.local/bin/cht.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+languaages=$(echo "bash sh golang rust lua cpp c csharp python" | tr ' ' '\n')
+core_utils=$(echo "xargs find mv sed awk" | tr ' ' '\n')
+
+selected=$(printf "$languaages\n$core_utils" | fzf)
+read -p "query: " query
+
+if echo $languaages | grep -qs $selected; then
+	tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+else
+	tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+fi
