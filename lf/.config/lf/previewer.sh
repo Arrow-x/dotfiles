0670ccb3c5020cfb29afd3d671dfb4fffaaa4290
Arrow-x <Houssem.kun26@gmail.com>
2023-05-15 19:17:47 +0100

    lf: update ueberzerg integration to u++

--- lf/.config/lf/previewer.sh
+++ lf/.config/lf/previewer.sh
@@ -1,81 +1,71 @@
 #!/bin/sh
-draw() {
-  ~/.config/lf/draw_img.sh "$@"
-  exit 1
+
+image() {
+    FILE_PATH="$1"
+    X=$4
+    Y=$5
+    MW=$(($2-1))
+    MH=$3
+    ueberzugpp cmd -s $UB_SOCKET -a add -i PREVIEW -x $X -y $Y --max-width $MW --max-height $MH -f "$FILE_PATH"
+    exit 1
 }
 
-hash() {
-  printf '%s/.cache/lf/%s' "$HOME" \
-    "$(stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' -- "$(readlink -f "$1")" | sha256sum | awk '{print $1}')"
+batorcat() {
+	file="$1"
+	shift
+	if command -v bat > /dev/null 2>&1
+	then
+		bat --color=always --style=plain --pager=never "$file" "$@"
+	else
+		cat "$file"
+	fi
 }
 
-cache() {
-  if [ -f "$1" ]; then
-    draw "$@"
-  fi
-}
+CACHE="$HOME/.cache/lf/$(stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' -- "$(readlink -f "$1")" | sha256sum | awk '{print $1}')"
 
-file="$1"
-shift
-
-if [ -n "$FIFO_UEBERZUG" ]; then
-  # case "$(file -Lb --mime-type -- "$file")" in
-  case "$(mimetype -L -b "$file")" in
-    image/gif)
-      cache="$(hash "$file").jpg"
-      cache "$cache" "$@"
-      ffmpegthumbnailer -i "$file" -o "$cache" -s 512
-      draw "$cache" "$@"
-      ;;
-    image/*)
-      cache="$(hash "$file").jpg"
-      cache "$cache" "$@"
-      convert -- "$file" -auto-orient -resize 512x512 "$cache"
-      draw "$cache" "$@"
-      ;;
-    video/*)
-      cache="$(hash "$file").jpg"
-      cache "$cache" "$@"
-      ffmpegthumbnailer -m -i "$file" -o "$cache" -s 512
-      draw "$cache" "$@"
-      ;;
-    audio/*)
-      cache="$(hash "$file").jpg"
-      cache "$cache" "$@"
-      ffmpegthumbnailer -i "$file" -o "$cache" -s 265
-      draw "$cache" "$@"
-      ;;
-    application/epub+zip)
-      cache="$(hash "$file").jpg"
-      cache "$cache" "$@"
-      gnome-epub-thumbnailer "$file" "$cache"
-      draw "$cache" "$@"
-	    ;;
-    application/pdf)
-      cache="$(hash "$file").jpg"
-      cache "$cache" "$@"
-	    gs -o "$cache" -sDEVICE=pngalpha -dLastPage=1 "$file" >/dev/null
-      draw "$cache" "$@"
-	    ;;
-    application/x-blender)
-      cache="$(hash "$file").jpg"
-      cache "$cache" "$@"
-      blender-thumbnailer "$file" "$cache"
-      draw "$cache" "$@"
-      ;;
-    application/x-compressed-tar) tar tf "$file" ;;
-    application/zip) unzip -l "$file" ;;
-    application/vnd.rar) 7z l "$file" ;;
-    application/x-7z-compressed) 7z l "$file" ;;
-    application/x-gdscript) bat -l go -n --color always --wrap never "$file" ;;
-    application/x-sharedlib) echo bin;;
-    application/x-executable) echo bin;;
-    application/x-object) echo bin;;
-    application/*) bat -n --color always --wrap never "$file" ;;
-    text/markdown) glow "$file";;
-    text/*) bat -n --color always --wrap never "$file" ;;
-  esac
-fi
-
-file -Lb -- "$1" | fold -s -w "$width"
+case "$(printf "%s\n" "$(readlink -f "$1")" | awk '{print tolower($0)}')" in
+	*.tgz|*.tar.gz) tar tzf "$1" ;;
+	*.tar.bz2|*.tbz2) tar tjf "$1" ;;
+	*.tar.txz|*.txz) xz --list "$1" ;;
+	*.tar) tar tf "$1" ;;
+	*.zip|*.jar|*.war|*.ear|*.oxt) unzip -l "$1" ;;
+	*.rar) unrar l "$1" ;;
+	*.7z) 7z l "$1" ;;
+	*.[1-8]) man "$1" | col -b ;;
+	*.o) nm "$1";;
+	*.torrent) transmission-show "$1" ;;
+	*.iso) iso-info --no-header -l "$1" ;;
+	*.odt|*.ods|*.odp|*.sxw) odt2txt "$1" ;;
+	*.doc) catdoc "$1" ;;
+	*.docx) docx2txt "$1" - ;;
+	*.xls|*.xlsx)
+		ssconvert --export-type=Gnumeric_stf:stf_csv "$1" "fd://1" | batorcat --language=csv
+		;;
+	*.pdf)
+		[ ! -f "${CACHE}.jpg" ] && \
+			pdftoppm -jpeg -f 1 -singlefile "$1" "$CACHE"
+		image "${CACHE}.jpg" "$2" "$3" "$4" "$5"
+		;;
+	*.epub)
+		[ ! -f "${CACHE}.jpg" ] && \
+            gnome-epub-thumbnailer "$1" "$CACHE".jpg
+		image "${CACHE}.jpg" "$2" "$3" "$4" "$5"
+		;;
+	*.avi|*.mp4|*.wmv|*.dat|*.3gp|*.ogv|*.mkv|*.mpg|*.mpeg|*.vob|*.fl[icv]|*.m2v|*.mov|*.webm|*.ts|*.mts|*.m4v|*.r[am]|*.qt|*.divx|*.wav|*.mp3|*.flac|*.m4a|*.wma|*.ape|*.ac3|*.og[agx]|*.spx|*.opus|*.as[fx]|*.mka)
+		[ ! -f "${CACHE}.jpg" ] && \
+			ffmpegthumbnailer -i "$1" -o "${CACHE}.jpg" -m -s 0 -q 5
+		image "${CACHE}.jpg" "$2" "$3" "$4" "$5"
+		;;
+	*.bmp|*.jpg|*.jpeg|*.png|*.xpm|*.webp|*.gif|*.jfif)
+		image "$1" "$2" "$3" "$4" "$5"
+		;;
+	*.svg)
+    		[ ! -f "${CACHE}.jpg" ] && \
+      		convert "$1" "${CACHE}.jpg"
+    		image "${CACHE}.jpg" "$2" "$3" "$4" "$5"
+    		;;
+	*)
+		batorcat "$1"
+		;;
+esac
 exit 0
