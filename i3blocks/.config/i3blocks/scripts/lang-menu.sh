a7a279e6ae8469e844aed355c9f988c3eb45f685
Arrow-x <Houssem.kun26@gmail.com>
2023-05-22 05:32:40 +0100

    i3blocks: removed uselss scripts, activate scirpts with less dependencies

--- i3blocks/.config/i3blocks/scripts/lang-menu.sh
+++ i3blocks/.config/i3blocks/scripts/lang-menu.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-entries="English\nArabic"
+entries="English (US)\nArabic"
 
 selected=$(echo -e "$entries" | wofi --conf=$HOME/.config/wofi/config.lang)
 
 case "$selected" in
-  "English")
-    swaymsg input type:keyboard xkb_switch_layout 0 >/dev/null; echo "  En";;
+  "English (US)")
+    swaymsg input type:keyboard xkb_switch_layout 0 >/dev/null;;
   "Arabic")
-    swaymsg input type:keyboard xkb_switch_layout 1 >/dev/null; echo "  Ar";;
+    swaymsg input type:keyboard xkb_switch_layout 1 >/dev/null;;
 esac
