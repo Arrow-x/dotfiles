a10cce09f42e2ac8ae9fc124b35288bbf7db2e66
Arrow-x <Houssem.kun26@gmail.com>
2023-06-03 15:06:55 +0100

    Scripts: cht.sh: languages and core utils are not hardcoded now

--- Scripts/.local/bin/cht.sh
+++ Scripts/.local/bin/cht.sh
@@ -1,24 +1,35 @@
 #!/bin/sh
-
-languages=$(echo "bash sh golang rust lua cpp c csharp python" | tr ' ' '\n')
-core_utils=$(echo "xargs curl jq find mv sed awk printf" | tr ' ' '\n')
 local_copy="$XDG_CACHE_HOME/cht_sh/"
-selected=$(printf "%s\n%s" "$languages" "$core_utils" | fzf)
+mode="$(printf "Language\nCore-util" | fzf)"
+
+if [ -z "$mode" ]; then
+	exit
+fi
+
+case "$mode" in
+	Language)
+		printf "What Language: " ;;
+	"Core-util")
+		printf "What Core-util: " ;;
+esac
+
+read -r selected
 
 if [ -z "$selected" ]; then
 	exit 1
 fi
 
-printf "Query: " >&2
+printf "Query: "
 read -r query
 
 file="$local_copy""$selected"-"$query"
 
-if printf "%s" "$languages" | grep -qs "$selected"; then
-	curl -s cht.sh/"$selected"/"$query"> "$file"-new
-else
-	curl -s cht.sh/"$selected"~"$query"> "$file"-new
-fi
+case "$mode" in
+	Language)
+		curl -s cht.sh/"$selected"/"$query"> "$file"-new ;;
+	"Core-util")
+		curl -s cht.sh/"$selected"~"$query"> "$file"-new ;;
+esac
 
 if [ -z "$(cat "$file"-new)" ]; then
 	rm "$file"-new
