ded0523dadf484917154b03f3ebc10cf0282c333
Arrow-x <Houssem.kun26@gmail.com>
2023-06-08 21:17:03 +0100

    Sway: lock before sleep, and sleep after a while

--- sway/.config/sway/scripts/lock.sh
+++ sway/.config/sway/scripts/lock.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+if [ -n "$(pgrep swaylock)" ]; then
+	exit
+fi
 PICTURE=/tmp/swaylock.png
-SCREENSHOT="grim -s 0.3 $PICTURE"
 
 BLANK='#00000000'
 CLEAR='#ffffff22'
@@ -10,29 +12,31 @@ WRONG='#880000bb'
 VERIFYING='#bb00bbbb'
 BLUR="10"
 
-$SCREENSHOT
+grim -s 0.3 $PICTURE
 convert "$PICTURE" -blur "$BLUR" "$PICTURE"
 # Times the screen off and puts it to background
-swayidle \
-    timeout  300 'swaymsg "output * power off"' \
-    resume 'swaymsg "output * power on"' &
+if [ -z "$(pgrep swayidle)" ]; then
+	swayidle \
+		timeout  300 'swaymsg "output * power off"' \
+		resume 'swaymsg "output * power on"' &
+fi
 # Locks the screen immediately
-swaylock --image "$PICTURE"\
-    --inside-ver-color=$CLEAR     \
-    --ring-ver-color=$VERIFYING   \
-    \
-    --inside-wrong-color=$CLEAR   \
-    --ring-wrong-color=$WRONG     \
-    \
-    --inside-color=$BLANK        \
-    --ring-color=$DEFAULT        \
-    --line-color=$BLANK          \
-    --separator-color=$DEFAULT   \
-    \
-    --text-ver-color=$TEXT          \
-    --text-wrong-color=$TEXT          \
-    --key-hl-color=$WRONG         \
-    --bs-hl-color=$WRONG
+swaylock -f --image "$PICTURE"\
+	--inside-ver-color=$CLEAR     \
+	--ring-ver-color=$VERIFYING   \
+	\
+	--inside-wrong-color=$CLEAR   \
+	--ring-wrong-color=$WRONG     \
+	\
+	--inside-color=$BLANK        \
+	--ring-color=$DEFAULT        \
+	--line-color=$BLANK          \
+	--separator-color=$DEFAULT   \
+	\
+	--text-ver-color=$TEXT          \
+	--text-wrong-color=$TEXT          \
+	--key-hl-color=$WRONG         \
+	--bs-hl-color=$WRONG
 # Kills last background task so idle timer doesn't keep running
 rm "$PICTURE"
 kill %%
