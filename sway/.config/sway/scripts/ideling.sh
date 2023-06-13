0284cd88838fd7b587b467909d68c077c194c8e3
Arrow-x <Houssem.kun26@gmail.com>
2023-06-13 21:48:37 +0100

    Scripts: sway: proj and ideling scripts don't stick around

--- sway/.config/sway/scripts/ideling.sh
+++ sway/.config/sway/scripts/ideling.sh
@@ -6,4 +6,4 @@ swayidle -w \
          timeout 300 '/home/arrowx/.config/sway/scripts/lock.sh' \
          timeout 700 'swaymsg "output * power off"' resume 'swaymsg "output * power on"' \
          timeout 1000 'systemctl suspend' resume 'swaymsg "output * power on"' \
-         before-sleep '/home/arrowx/.config/sway/scripts/lock.sh'
+         before-sleep '/home/arrowx/.config/sway/scripts/lock.sh' &
