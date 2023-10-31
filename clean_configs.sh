159d9ac8f110799e9d517a42011bb2e2c68bf30d
Arrow-x <Houssem.kun26@gmail.com>
2023-10-31 13:20:00 +0100

    Scripts: rewrite clean install script

---
+++ clean_configs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+dirs="$(find ~/dotfiles -maxdepth 1 -mindepth 1 -type d -not -path "*/.git*")"
+for dir in $dirs; do
+	echo "Removing $dir"
+	stow -D "$(echo "$dir" | awk -F "/" '{print $5}')"
+done
