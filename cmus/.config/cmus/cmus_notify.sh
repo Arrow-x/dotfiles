0020c1e87990c4de7b388f30386c6a3b4340cc1f
Arrow-x <Houssem.kun26@gmail.com>
2024-01-06 13:26:44 +0100

    cmus: update my scripts

--- cmus/.config/cmus/cmus_notify.sh
+++ cmus/.config/cmus/cmus_notify.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # TODO a default icon when no album art
-# TODO Use XDG paths
 # TODO put all the tags possible in variables
 
 cmus_query="$(cmus-remote --query)"
@@ -10,23 +9,20 @@ title="$(echo "$cmus_query" | grep "tag title" | sed "s/tag title //")"
 artist="$(echo "$cmus_query" | grep "tag artist" | sed "s/tag artist //")"
 album="$(echo "$cmus_query" | grep "tag album\b" | sed 's/tag album //')"
 
-echo "$cmus_query" | grep "file" | sed "s/file //" | sed 's/"/\\"/g' | sed 's/$/"/;s/^/"/' | xargs -I {} ffmpegthumbnailer -i "{}" -o ~/.cache/cmus_notification_cover.jpg
+echo "$cmus_query" | grep "file" | sed "s/file //" | sed 's/"/\\"/g' | sed 's/$/"/;s/^/"/' | xargs -I {} ffmpegthumbnailer -i "{}" -o "$XDG_CACHE_HOME"/cmus_notification_cover.jpg
 
-if [ -z "$artist" ];
-then
+if [ -z "$artist" ]; then
 	artist="Unknown"
 fi
 
-if [ -z "$album" ];
-then
+if [ -z "$album" ]; then
 	album="Unknown"
 fi
 
-if [ -z "$title" ];
-then
+if [ -z "$title" ]; then
 	title="$(echo "$cmus_query" | grep "file" | sed "s/file //" | sed 's/"/\\"/g' | sed 's/$/"/;s/^/"/' | xargs basename)"
 fi
 
-notify-send -u low --icon=~/.cache/cmus_notification_cover.jpg "Now Playing:" "<b>$title</b>\nby: <b>$artist</b> from: <b>$album</b>"
+notify-send -u low --icon="$XDG_CACHE_HOME"/cmus_notification_cover.jpg "Now Playing:" "<b>$title</b>\nby: <b>$artist</b> from: <b>$album</b>"
 
-rm ~/.cache/cmus_notification_cover.jpg
+rm "$XDG_CACHE_HOME"/cmus_notification_cover.jpg
