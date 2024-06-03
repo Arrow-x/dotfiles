70cdfb013769407a6a29219fa721ce91467c5dac
Arrow-x <Houssem.kun26@gmail.com>
2024-06-04 00:59:49 +0100

    clean

--- lf/.config/lf/clear_img.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-case "$TERM" in
-	foot | xterm-256color | tmux-256color) ueberzugpp cmd -s "$UB_SOCKET" -a remove -i PREVIEW -d true ;;
-	*) ueberzugpp cmd -s "$UB_SOCKET" -a remove -i PREVIEW ;;
-esac
