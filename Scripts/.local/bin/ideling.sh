7dcd36a5264fd364b52cd32e0a8e6b69be197d36
Arrow-x <Houssem.kun26@gmail.com>
2024-11-28 17:38:50 +0100

    Scripts: move scripts

--- Scripts/.local/bin/ideling.sh
+++ Scripts/.local/bin/ideling.sh
@@ -5,14 +5,14 @@ fi
 
 if [ "$XDG_CURRENT_DESKTOP" = "Hyprland" ]; then
 	swayidle -w \
-		timeout 300 '/home/arrowx/.config/sway/scripts/lock.sh' \
+		timeout 300 "$HOME/.local/bin/lock.sh" \
 		timeout 700 'hyprctl dispatch dpms off' resume ' hyprctl dispatch dpms on' \
 		timeout 1000 'systemctl suspend' resume 'swaymsg "output * power on"' \
 		before-sleep '/home/arrowx/.config/sway/scripts/lock.sh' &
 
 else
 	swayidle -w \
-		timeout 300 '/home/arrowx/.config/sway/scripts/lock.sh' \
+		timeout 300 "$HOME/.local/bin/lock.sh" \
 		timeout 700 'swaymsg "output * power off"' resume 'swaymsg "output * power on"' \
 		timeout 1000 'systemctl suspend' resume 'swaymsg "output * power on"' \
 		before-sleep '/home/arrowx/.config/sway/scripts/lock.sh' &
---
+++ Scripts/.local/bin/lock.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+PICTURE=/tmp/swaylock.png
+
+BLANK='#00000000'
+CLEAR='#ffffff22'
+DEFAULT='#ff00ffcc'
+TEXT='#ee00eeee'
+WRONG='#880000bb'
+VERIFYING='#bb00bbbb'
+BLUR="10"
+
+grim -s 0.3 $PICTURE
+convert "$PICTURE" -blur "$BLUR" "$PICTURE"
+
+if [ -n "$(pgrep swaylock)" ]; then
+	exit
+fi
+# Times the screen off and puts it to background
+if [ -z "$(pgrep swayidle)" ]; then
+	swayidle \
+		timeout 300 'swaymsg "output * power off"' \
+		resume 'swaymsg "output * power on"' &
+fi
+# Locks the screen immediately
+swaylock -f --image "$PICTURE" \
+	--inside-ver-color=$CLEAR \
+	--ring-ver-color=$VERIFYING \
+	\
+	--inside-wrong-color=$CLEAR \
+	--ring-wrong-color=$WRONG \
+	\
+	--inside-color=$BLANK \
+	--ring-color=$DEFAULT \
+	--line-color=$BLANK \
+	--separator-color=$DEFAULT \
+	\
+	--text-ver-color=$TEXT \
+	--text-wrong-color=$TEXT \
+	--key-hl-color=$WRONG \
+	--bs-hl-color=$WRONG
+# Kills last background task so idle timer doesn't keep running
+kill %%
+rm "$PICTURE"
