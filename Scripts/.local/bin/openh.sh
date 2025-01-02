052f88e36c692cacd5dc98d01805e2ccc0e81da0
Arrow-x <Houssem.kun26@gmail.com>
2025-01-02 17:56:12 +0100

    need a better way to pass the location

--- Scripts/.local/bin/openh.sh
+++ Scripts/.local/bin/openh.sh
@@ -2,19 +2,7 @@
 SHELL=/bin/bash
 opener="nsxiv"
 
-find_hentai() {
-	DOJ=$(find ~/File/Homework/H-Manga/Doujinshi/ -mindepth 1 -type d -printf "%T+ %p\n")
-	PAR=$(find ~/File/Homework/H-Manga/Parody/ -mindepth 1 -type d -printf "%T+ %p\n")
-	TAN=$(find ~/File/Homework/H-Manga/Tankebons/ -mindepth 1 -type d -printf "%T+ %p\n")
-	list=""
-	[ -n "$DOJ" ] && list="$(printf "%s\n" "$DOJ")"
-	[ -n "$PAR" ] && list="$(printf "%s\n%s" "$list" "$PAR")"
-	[ -n "$TAN" ] && list="$(printf "%s\n%s" "$list" "$TAN")"
-	printf "%s" "$list" | sort -r | sed "s#.*/home#/home#"
-}
-export -f find_hentai
-
-find_hentai | fzf \
+find ~/gallery-dl/nhentai/ -mindepth 1 -type d -printf "%T+ %p\n" | sort -r | sed "s#.*/home#/home#" | fzf \
 	--preview='draw_thumb {} $FZF_PREVIEW_COLUMNS $FZF_PREVIEW_LINES' \
 	--bind "ctrl-l:execute: setsid -f $opener {}>/dev/null" \
 	--bind "ctrl-d:reload(gio trash {} && find_hentai)" \
