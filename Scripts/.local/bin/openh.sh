0f447a870efde14d5a550e4c1652feea02e190d6
Arrow-x <Houssem.kun26@gmail.com>
2025-05-24 12:14:11 +0100

    Scripts: download_managers/openh.sh: support exhentai

--- Scripts/.local/bin/openh.sh
+++ Scripts/.local/bin/openh.sh
@@ -2,7 +2,7 @@
 SHELL=/bin/bash
 opener="nsxiv"
 
-find ~/gallery-dl/nhentai/ -mindepth 1 -type d -printf "%T+ %p\n" | sort -r | cut -d' ' -f2- | fzf \
+find ~/gallery-dl/nhentai/ ~/gallery-dl/exhentai/ -mindepth 1 -type d -printf "%T+ %p\n" | sort -r | cut -d' ' -f2- | fzf \
 	--preview='draw_thumb {} $FZF_PREVIEW_COLUMNS $FZF_PREVIEW_LINES' \
 	--bind "ctrl-l:execute: setsid -f $opener {}>/dev/null" \
 	--bind "ctrl-d:reload(gio trash {} && find_hentai)" \
