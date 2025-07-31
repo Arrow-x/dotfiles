f2be75c24f778b2fdafcda764d688102d0eeae73
Arrow-x <Houssem.kun26@gmail.com>
2025-07-31 15:36:26 +0100

    Scripts: set binger wallpaper if exist, formating, and nsxiv play gifs

--- Scripts/.local/bin/openh.sh
+++ Scripts/.local/bin/openh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SHELL=/bin/bash
-opener="nsxiv"
+opener="nsxiv -a"
 
 find $HOME/gallery-dl/nhentai/ $HOME/gallery-dl/exhentai/ -mindepth 1 -type d -printf "%T+ %p\n" | sort -r | cut -d' ' -f2- | fzf \
 	--preview='draw_thumb {} $FZF_PREVIEW_COLUMNS $FZF_PREVIEW_LINES' \
