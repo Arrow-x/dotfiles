41fa24d621b9c55c45c1e7093373d2ad9f34867e
Arrow-x <Houssem.kun26@gmail.com>
2025-10-26 12:07:58 +0100

    systemd: an init script

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-systemctl --user enable --now pipewire
-systemctl --user enable --now pipewire-pulse.service
-systemctl enable --now keyd
-systemctl enable --now cronie
-systemctl enable ly.service
---
+++ systemd_init.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#TODO: put the install packages command here too, first the package manager then the packages on the official repos then the AUR packages
+
+#TODO: install /etc/ configs too here
+
+#TODO: at least install ly, keyd and opentabletdriver
+
+systemctl enable ly.service
+
+systemctl enable --now keyd
+systemctl enable --now cronie
+
+systemctl --user enable --now pipewire
+systemctl --user enable --now pipewire-pulse.service
+systemctl --user enable --now opentabletdriver
