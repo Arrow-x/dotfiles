8ae63085e63e621b0fb31a3d7d09b6f3bf467747
Arrow-x <Houssem.kun26@gmail.com>
2026-04-30 19:54:11 +0100

    misc

--- Scripts/.local/bin/lock.sh
+++ Scripts/.local/bin/lock.sh
@@ -1,40 +1,36 @@
 #!/bin/sh
-if [ "$XDG_CURRENT_DESKTOP" = "Hyprland" ]; then
-	setsid -f hyprlock
-else
-	PICTURE=/tmp/swaylock.png
+PICTURE=/tmp/swaylock.png
 
-	BLANK='#00000000'
-	CLEAR='#ffffff22'
-	DEFAULT='#ff00ffcc'
-	TEXT='#ee00eeee'
-	WRONG='#880000bb'
-	VERIFYING='#bb00bbbb'
-	BLUR="10"
+BLANK='#00000000'
+CLEAR='#ffffff22'
+DEFAULT='#ff00ffcc'
+TEXT='#ee00eeee'
+WRONG='#880000bb'
+VERIFYING='#bb00bbbb'
+BLUR="10"
 
-	grim -s 0.3 $PICTURE
-	magick "$PICTURE" -blur "$BLUR" "$PICTURE"
+grim -s 0.3 $PICTURE
+magick "$PICTURE" -blur "$BLUR" "$PICTURE"
 
-	if [ -n "$(pgrep swaylock)" ]; then
-		exit
-	fi
-	# Locks the screen immediately
-	swaylock -f --image "$PICTURE" \
-		--inside-ver-color=$CLEAR \
-		--ring-ver-color=$VERIFYING \
-		\
-		--inside-wrong-color=$CLEAR \
-		--ring-wrong-color=$WRONG \
-		\
-		--inside-color=$BLANK \
-		--ring-color=$DEFAULT \
-		--line-color=$BLANK \
-		--separator-color=$DEFAULT \
-		\
-		--text-ver-color=$TEXT \
-		--text-wrong-color=$TEXT \
-		--key-hl-color=$WRONG \
-		--bs-hl-color=$WRONG
-
-	rm "$PICTURE"
+if [ -n "$(pgrep swaylock)" ]; then
+	exit
 fi
+# Locks the screen immediately
+swaylock -f --image "$PICTURE" \
+	--inside-ver-color=$CLEAR \
+	--ring-ver-color=$VERIFYING \
+	\
+	--inside-wrong-color=$CLEAR \
+	--ring-wrong-color=$WRONG \
+	\
+	--inside-color=$BLANK \
+	--ring-color=$DEFAULT \
+	--line-color=$BLANK \
+	--separator-color=$DEFAULT \
+	\
+	--text-ver-color=$TEXT \
+	--text-wrong-color=$TEXT \
+	--key-hl-color=$WRONG \
+	--bs-hl-color=$WRONG
+
+rm "$PICTURE"
